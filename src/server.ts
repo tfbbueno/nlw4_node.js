fdbdf31c8921b51beb405e5258e9a5d5c5120e25
Thais <tfb.bueno@gmail.com>
2021-02-25 11:11:47 -0400

    feat:

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello!" });
+});
+
+app.post("/", (request, response) => {
+  return response.json({ message: "Post Funcionando!" });
+});
+
+app.listen(3333, () => console.log("servidor está rodando!"));
