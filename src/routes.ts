e044f93966edae50369eb82a0d69e0b8b84f4dad
Thais <tfb.bueno@gmail.com>
2021-02-25 22:01:07 -0400

    feat: aula 4 - envio de e-mails

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
+import { SendMailController } from "./controller/SendMailController";
 import { SurveysController } from "./controller/SurveysController";
 import { UserController } from "./controller/UserController";
 
 const router = Router();
 const userController = new UserController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
+
+router.post("/sendMail", sendMailController.execute);
+
 export { router };
